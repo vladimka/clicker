1e8a845d3a36a8a90473710e521bc068e2144e04
vladimka <zverevv.04@gmail.com>
2021-09-04 00:27:44 +0900

    c

--- js/index.js
+++ js/index.js
@@ -30,14 +30,61 @@ function load(){
                     bonusType : 'cps',
                     bonus : 1
                 }
-            ]
+            ],
+            autosaveEnabled : true,
+            autosaveInterval : 10
         }
 
     return save;
 }
 
+function toggleAutosave(){
+    user.autosaveEnabled = !user.autosaveEnabled;
+}
+
+function setAutosaveInterval(el){
+    user.autosaveInterval = parseInt(el.value);
+}
+
+function hardReset(){
+    user = {
+        balance : 0,
+        coinsPerClick : 1,
+        coinsPerSecond : 0,
+        upgrades : [
+            {
+                displayName : '+1 finger',
+                value : 0,
+                cost : 30,
+                costMultiplier : 1.17,
+                bonusType : 'cpc',
+                bonus : 1
+            },
+            {
+                displayName : 'auto finger',
+                value : 0,
+                cost : 200,
+                costMultiplier : 1.17,
+                bonusType : 'cps',
+                bonus : 1
+            }
+        ],
+        autosaveEnabled : true,
+        autosaveInterval : 10
+    }
+    save();
+    location.reload();
+}
+
 function btnLoad(){
     user = load();
+}
+
+function autosaveTimeout(){
+    if(user.autosaveEnabled)
+        save();
+    
+    setTimeout(autosaveTimeout, user.autosaveInterval * 1e3);
 }
 
 function doMoney(){
@@ -57,14 +104,16 @@ function doUpgrade(index){
         : user.coinsPerSecond += upgrade.bonus;
 }
 
-let upgradesTab = document.getElementById('upgrades');
+let upgradesTab = document.querySelector('.tab.upgrades-tab');
 let balanceSpan = document.getElementById('balance');
 let coinsPerClickSpan = document.getElementById('coinsPerClick');
 let coinsPerSecondSpan = document.getElementById('coinsPerSecond');
 let notificationsUl = document.getElementsByClassName('notifications')[0];
+let autosaveStatus = document.getElementById('autosaveStatus');
 let upgradeElements;
 
-function renderUpgrades(){
+function firstRender(){
+    document.getElementById('autosaveInterval').value = user.autosaveInterval;
     user.upgrades.forEach((upgrade, index) => {
         upgradesTab.innerHTML += `
             <div class='upgrade tab-item' id='${index}-upgrade'>
@@ -83,6 +132,7 @@ function redraw(){
     balanceSpan.innerText = user.balance.toFixed(2);
     coinsPerSecondSpan.innerText = user.coinsPerSecond.toFixed(2);
     coinsPerClickSpan.innerText = user.coinsPerClick.toFixed(2);
+    autosaveStatus.innerText = user.autosaveEnabled ? 'Enabled' : 'Disabled';
     upgradeElements.forEach(upgradeElement => {
         let upgradeId = parseInt(upgradeElement.id.split('-')[0]);
         let upgrade = user.upgrades[upgradeId];
@@ -107,7 +157,22 @@ class NotificationManager{
     }
 }
 
-renderUpgrades();
+function setTab(tabId){
+    document.querySelector('.tab.active').classList.remove('active');
+    document.querySelector('.tab-button.active').classList.remove('active');
+
+    document.querySelector(`.tab.${tabId}`).classList.add('active');
+    document.querySelector(`.tab-button.${tabId}`).classList.add('active');
+}
+
+function toggleTheme(){
+    if(document.body.classList.contains('dark'))
+        return document.body.classList.remove('dark');
+
+    document.body.classList.add('dark');
+}
+
+firstRender();
 setInterval(() => user.balance += user.coinsPerSecond, 1e3);
 setInterval(redraw, 100);
-setInterval(() => save(), 1e4);
+autosaveTimeout();
